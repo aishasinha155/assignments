69d3e4d32f6c35c8a0c1c0256e02fddcf3e118a7
aisha <aishasinha316@gmail.com>
2022-10-19 12:47:16 +0530

    sdfsd

---
+++ string.cpp/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include<cstring>
+using namespace std;
+int main(){
+    char s[50];
+    cout<<"Enter a string";
+    cin>>s;
+   
+    cout<<strlen(s)<<endl;
+    return 0;
+}
